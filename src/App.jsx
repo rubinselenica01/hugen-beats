889967d083c6a387db9e3a53ab50c4100a15992c
Rubin Selenica <selenicarubin@gmail.com>
2026-04-19 15:45:35 +0200

    Scroll to top of the page when redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { CartProvider } from './context/CartContext.jsx'
 import BeatsCatalogPage from './pages/BeatsCatalogPage.jsx'
 import HomePage from './pages/HomePage.jsx'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <CartProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
